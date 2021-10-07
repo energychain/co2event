16033322ec5e72e65a1e22fcce8b5bba4291b8fd
Thorsten Zoerner <me@thorsten-zoerner.com>
2021-10-07 23:23:40 +0200

    Redefined number of decimals

--- smartcontracts/src/co2accounting.sol
+++ smartcontracts/src/co2accounting.sol
@@ -13,9 +13,12 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract CO2Accounting is AccessControl, ERC20 {
     bytes32 public constant EMITTER_ROLE = keccak256("EMITTER_ROLE");
     bytes32 public constant COMPENSATOR_ROLE = keccak256("COMPENSATOR_ROLE");
+    uint256 public constant Decimals = 0;
 
-    constructor() ERC20("CO2A", "CO2A") {
+    constructor() ERC20("CO2G", "CO2G") {
       _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+      _setupRole(EMITTER_ROLE,msg.sender);
+      _setupRole(COMPENSATOR_ROLE,msg.sender);
     }
 
     function emission(address to, uint256 amount) public onlyRole(EMITTER_ROLE) {
